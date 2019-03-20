0e109dd777ca7c8bb3259da2941df05776dd80b0
Christoph Krey <c@ckrey.de>
2019-03-20 21:32:48 +0100

    [FIX] don't parse yaml on connect

--- check_beanstalkd.py
+++ check_beanstalkd.py
@@ -46,7 +46,7 @@ def main(argv):
             critical = int(arg)
 
     try:
-        beanstalk = beanstalkc.Connection(host=host, port=port)
+        beanstalk = beanstalkc.Connection(host=host, port=port, parse_yaml=False)
 
         if tube == None:
             #pprint(beanstalk.stats())
